9d2260b9370ed950389fb14dcb15eafd1b110243
Oz <o.lodriguez@gmail.com>
2016-11-06 15:28:49 +0100

    stuff

--- app/components/navigation/component.js
+++ app/components/navigation/component.js
@@ -1,3 +1,5 @@
+import './component.scss';
+
 export default class Navigation {
     constructor() {
         this.navigation = {
--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import ko from 'knockout';
 import Navigation from './components/navigation/component';
 import GoogleMaps from './components/google-maps/component';
 
+import './app.scss';
+
 class App {
     constructor() {
         this.nav = new Navigation();
