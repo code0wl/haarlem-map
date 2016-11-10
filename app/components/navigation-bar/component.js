2013b167648057b8da9791c340fb15d7510263c6
Oz <o.lodriguez@gmail.com>
2016-11-10 09:21:42 +0100

    UI rework

--- app/components/navigation-bar/component.js
+++ app/components/navigation-bar/component.js
@@ -3,18 +3,7 @@ import './component.scss';
 export default class Navigation {
     constructor() {
         this.navigation = [
-            {
-                label: 'navigation1',
-                link: 'some link'
-            },
-            {
-                label: 'navigation2',
-                link: 'some link'
-            },
-            {
-                label: 'navigation3',
-                link: 'some link'
-            }
+            { label: 'menu', clazz: 'fa fa-bars' }
         ];
     }
 } 
